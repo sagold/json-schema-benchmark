3c31b11ff5979ce9eb1d1cb2541571f263d7a01b
Sascha Goldhofer <post@saschagoldhofer.de>
2022-04-23 18:45:52 +0200

    change: draft-07 to be default draft

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const testRunner = require("./testRunner");
 const validators = require("./validators");
 
 (async () => {
-  const draft04Validators = await validators(
-    "http://json-schema.org/draft-04/schema",
-    "4"
+  const draft07Validators = await validators(
+    "http://json-schema.org/draft-07/schema",
+    "7"
   );
   const draft06Validators = await validators(
     "http://json-schema.org/draft-06/schema",
     "6"
   );
-  const draft07Validators = await validators(
-    "http://json-schema.org/draft-07/schema",
-    "7"
+  const draft04Validators = await validators(
+    "http://json-schema.org/draft-04/schema",
+    "4"
   );
 
   await testRunner({
     validators: draft07Validators,
     schemaVersion: "draft7",
-    folder: "draft7/",
+    folder: "",
   });
   await testRunner({
     validators: draft06Validators,
     schemaVersion: "draft6",
-    folder: "",
+    folder: "draft6",
   });
   await testRunner({
     validators: draft04Validators,
     schemaVersion: "draft4",
-    folder: "draft4/",
+    folder: "draft4",
   });
 })();
